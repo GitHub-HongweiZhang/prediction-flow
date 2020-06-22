3e4d9bab535f3228fe40bb4820fc277a415bafee
張洪偉 <Hongwei Zhang>
2020-06-22 16:29:49 +0900

    fix deepFM

--- prediction_flow/pytorch/nn/fm.py
+++ prediction_flow/pytorch/nn/fm.py
@@ -13,11 +13,14 @@ import torch.nn as nn
 class FM(nn.Module):
     """FM layer.
     """
-    def __init__(self):
+    def __init__(self, reduce_sum=True):
         super(FM, self).__init__()
+        self.reduce_sum = reduce_sum
 
     def forward(self, x):
-        sum_squared = torch.pow(torch.sum(x, dim=1), 2).unsqueeze(1)
-        squared_sum = torch.sum(torch.pow(x, 2), dim=1).unsqueeze(1)
-        second_order = 0.5 * (sum_squared - squared_sum)
-        return second_order
+        sum_squared = torch.pow(torch.sum(x, dim=1), 2)
+        squared_sum = torch.sum(torch.pow(x, 2), dim=1)
+        second_order = sum_squared - squared_sum
+        if self.reduce_sum:
+            output = torch.sum(second_order, dim=1, keepdim=True)
+        return 0.5 * output
